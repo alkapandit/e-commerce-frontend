255189a8a416db7dc98dc99ac7d9009960ad07a8
Alka Pandit <alkapandit6101998@gmail.com>
2026-07-11 16:46:33 +0530

    add: shadcn setup done, also using pnpm instead of npm

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-slate-100">
       <h1 className="text-4xl font-bold text-blue-600">
         Tailwind CSS is working!
+        <Button>This is Shadcn button </Button>
       </h1>
     </div>
   );
